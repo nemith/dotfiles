c62c3029f3d83c1104c9e0c54ae3cc55f5851091
Brandon Bennett <bennetb@gmail.com>
2013-11-11 12:58:59 -0700

    Add utility for switching java versions

--- home/.bashrc.d/os/darwin.bash
+++ home/.bashrc.d/os/darwin.bash
@@ -52,3 +52,13 @@ fi
 
 # Quick alias for updating all homebrewed apps
 alias update="brew update && brew upgrade"
+
+
+# Function for switching java 
+alias java_ls='/usr/libexec/java_home -V 2>&1 | grep -E "\d.\d.\d_\d\d" | cut -d , -f 1 | colrm 1 4 | grep -v Home'
+
+function java_use() {
+    export JAVA_HOME=$(/usr/libexec/java_home -v $1)
+    export PATH=$JAVA_HOME/bin:$PATH
+    java -version
+}
