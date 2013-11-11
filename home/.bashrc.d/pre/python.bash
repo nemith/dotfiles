51490e312d7ed856d96911e02a6dc85d26306ef3
Brandon Bennett <bennetb@gmail.com>
2013-11-11 12:59:13 -0700

    Add alias to upgrade all pip pacakages

--- home/.bashrc.d/pre/python.bash
+++ home/.bashrc.d/pre/python.bash
@@ -5,3 +5,4 @@ if [[ -x $(which virtualenvwrapper.sh) ]]; then
 	source $(which virtualenvwrapper.sh)
 fi
 
+alias pip_upgrade_all="pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip install -U"
