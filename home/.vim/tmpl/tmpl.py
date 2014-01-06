8e50c2c54087712929334def86a54471c55447e6
Brandon Bennett <bennetb@gmail.com>
2014-01-06 13:24:23 -0800

    Add vim dotfiles

---
+++ home/.vim/tmpl/tmpl.py
@@ -0,0 +1,6 @@
+# copyright 2004-present facebook. all rights reserved.
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
