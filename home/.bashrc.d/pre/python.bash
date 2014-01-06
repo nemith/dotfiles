e94b8e418e07096a516200bd293822ec9fac2b32
Brandon Bennett <bennetb@gmail.com>
2014-01-06 13:23:32 -0800

    Fix whiff and use whiff for virtualenvwrapper

--- home/.bashrc.d/pre/python.bash
+++ home/.bashrc.d/pre/python.bash
@@ -1,8 +1,6 @@
-# TODO: Check for 
-
 # If we have virtualenvwrapper source it now
-if [[ -x $(which virtualenvwrapper.sh) ]]; then
-	source $(which virtualenvwrapper.sh)
+if [[ -x $(whiff virtualenvwrapper.sh) ]]; then
+	source $(whiff virtualenvwrapper.sh)
 fi
 
 alias pip_upgrade_all="pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip install -U"
