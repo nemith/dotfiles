570ab59578a33779d96149b80847be9a76ac4ff7
Brandon Bennett <bennetb@gmail.com>
2014-01-06 13:25:08 -0800

    Add facebook specific stuff (needs to move)

---
+++ home/.bashrc.d/pre/facebook.bash
@@ -0,0 +1,8 @@
+# Source Facebook definitions
+if [ -f /mnt/vol/engshare/admin/scripts/master.bashrc ]; then
+	. /mnt/vol/engshare/admin/scripts/master.bashrc
+fi
+
+source /etc/bashrc
+source /mnt/vol/engshare/admin/scripts/master.bashrc
+source /home/engshare/admin/scripts/ssh/manage_agent.sh
