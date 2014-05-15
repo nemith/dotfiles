5d6a477d0029596c5d725e44be88c94426e9fe6d
Brandon Bennett <bennetb@gmail.com>
2014-05-15 14:41:38 -0700

    Add fbhostname function

--- home/.bashrc.d/pre/facebook.bash
+++ home/.bashrc.d/pre/facebook.bash
@@ -6,3 +6,12 @@ source /etc/bashrc
 source /mnt/vol/engshare/admin/scripts/master.bashrc
 source /home/engshare/admin/scripts/ssh/manage_agent.sh
 source "$ADMIN_SCRIPTS"/ssh/manage_rootcanal.sh
+
+
+function fbhostname {
+    host=$1
+    [ "$host" == "" ] && host=$(hostname)
+
+    # remove known tlds
+    echo $(echo $host | sed 's/\.\(facebook.com\|thefacebook.com\|tfbnw.net\)$//')
+}
