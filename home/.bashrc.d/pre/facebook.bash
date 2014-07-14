06ff982310a1d814b104d1f1c2b0d046a92c0faf
Brandon Bennett <bbennett@fb.com>
2014-07-14 14:17:09 -0700

    Update FB scripts

--- home/.bashrc.d/pre/facebook.bash
+++ home/.bashrc.d/pre/facebook.bash
@@ -1,12 +1,10 @@
-# Continue if not on fb server
+# skip if not on fb server
 [ ! -f /etc/fbwhoami ] && return
 
 # Source Facebook definitions
 source /etc/bashrc
 source /mnt/vol/engshare/admin/scripts/master.bashrc
 source /home/engshare/admin/scripts/ssh/manage_agent.sh
-source "$ADMIN_SCRIPTS"/ssh/manage_rootcanal.sh
-
 
 function fbhostname {
     host=$1
