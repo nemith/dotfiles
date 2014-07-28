9f3600851623329d3a0ab5b7f0beae03f679b51d
Brandon Bennett <bbennett@fb.com>
2014-07-28 13:32:08 -0700

    Golang config for fb

--- home/.bashrc.d/pre/facebook.bash
+++ home/.bashrc.d/pre/facebook.bash
@@ -5,6 +5,8 @@
 source /etc/bashrc
 source /mnt/vol/engshare/admin/scripts/master.bashrc
 source /home/engshare/admin/scripts/ssh/manage_agent.sh
+ 
+export FBCODE=$HOME/fbcode
 
 function fbhostname {
     host=$1
@@ -12,4 +14,29 @@ function fbhostname {
 
     # remove known tlds
     echo $(echo $host | sed 's/\.\(facebook.com\|thefacebook.com\|tfbnw.net\)$//')
+}
+
+# Set gopath if we are on our devserver
+if [ -d /data/users/$USER/go ]; then 
+    export GOPATH=/data/users/$USER/go
+    append_path $GOPATH/bin
+fi
+
+
+function fbgopath {
+    shopt -s nullglob
+
+    # Remove all old fbcode gopaths
+    GOPATH=$(echo $GOPATH | sed "s,$FBCODE/[^:]*:\?,,g")
+    GOPATH=${GOPATH/%:/}
+
+    for project in $(fbmake what | cut -d ' ' -f2); do
+            project_build_dir="$FBCODE/_build/dbg/$project"
+            for golive_linktree_dir in $project_build_dir/*.golive.linktree; do
+            GOPATH=$GOPATH:$golive_linktree_dir
+            done
+    done
+
+    echo $GOPATH
+    export GOPATH
 }
