b1250fff7cf71da096f76a499ce3f658ee8aa442
Brandon Bennett <bennetb@gmail.com>
2015-02-09 21:22:02 -0700

    [powerline] be smarter with python paths

--- home/.bashrc.d/60powerline.bash
+++ home/.bashrc.d/60powerline.bash
@@ -1,8 +1,5 @@
-PY_USER_SITE=$(python -m site --user-site)
-PY_SYSTEM_SITE=$(python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()")
-
-for pydir in $PY_USER_SITE $PY_SYSTEM_SITE; do
-    if [ -d $pydir/powerline ]; then
+for pydir in $(python -c "import sys; print ' '.join(sys.path[1:])"); do
+	if [ -d $pydir/powerline ]; then
         export POWERLINE_DIR=$pydir/powerline
 
         # Powerline for bash
