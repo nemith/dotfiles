158879de8690f31459d8046b389d870bc2652c50
Brandon Bennett <bennetb@gmail.com>
2015-10-31 21:24:15 -0600

    [powerline] More advanced lookup for powerline

--- home/.bashrc.d/60powerline.bash
+++ home/.bashrc.d/60powerline.bash
@@ -1,9 +1,10 @@
-for pydir in $(python -c "import sys; print ' '.join(sys.path[1:])"); do
-	if [ -d $pydir/powerline ]; then
-        export POWERLINE_DIR=$pydir/powerline
-
-        # Powerline for bash
-        source $POWERLINE_DIR/bindings/bash/powerline.sh
+powerline_dirs=(
+ 	'/usr/share/powerline'
+	$(python -c "import sys; print '/powerline '.join(sys.path[1:])")
+)
+for dir in ${powerline_dirs[@]}; do
+	if [ -f $dir/bindings/bash/powerline.sh ]; then
+        source $dir/bindings/bash/powerline.sh
         break;
     fi
 done
