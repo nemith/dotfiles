551642e4a1f9fa8bb2ca41af7253cb497c41592e
Brandon Bennett <bennetb@gmail.com>
2016-03-11 16:07:24 -0700

    Experiment with fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -3,6 +3,7 @@ set powerline_dirs /usr/share/powerline (python -c "import sys; print '/powerlin
 for dir in $powerline_dirs
 	set plfunc $dir/bindings/fish/powerline-setup.fish
     if test -e $plfunc
+		set -x POWERLINE_DIR $dir
 		source $plfunc
 		powerline-setup
 		break
