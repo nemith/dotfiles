d0bfea65f72261ca65e1ba1453be29404b641b1d
Brandon Bennett <bennetb@gmail.com>
2016-03-11 16:14:23 -0700

    [bash/powerline] export POWERLINE_DIR

--- home/.bashrc.d/60powerline.bash
+++ home/.bashrc.d/60powerline.bash
@@ -4,6 +4,7 @@ powerline_dirs=(
 )
 for dir in ${powerline_dirs[@]}; do
 	if [ -f $dir/bindings/bash/powerline.sh ]; then
+		export POWERLINE_DIR=$dir
         source $dir/bindings/bash/powerline.sh
         break;
     fi
