ec398c2a1096c5ad165380f59e9a2dd659b8c6b0
Brandon Bennett <bbennett@fb.com>
2016-03-16 12:10:17 -0700

    [powerline] fix directory discovery

--- home/.bashrc.d/60powerline.bash
+++ home/.bashrc.d/60powerline.bash
@@ -1,11 +1,11 @@
 powerline_dirs=(
+	$(python -c "import sys; print ' '.join([p+'/powerline' for p in sys.path[1:]])")
  	'/usr/share/powerline'
-	$(python -c "import sys; print '/powerline '.join(sys.path[1:])")
 )
-for dir in ${powerline_dirs[@]}; do
-	if [ -f $dir/bindings/bash/powerline.sh ]; then
+for dir in "${powerline_dirs[@]}"; do
+	if [ -d "$dir" ]; then
 		export POWERLINE_DIR=$dir
-        source $dir/bindings/bash/powerline.sh
-        break;
-    fi
+    source "$POWERLINE_DIR/bindings/bash/powerline.sh"
+    break;
+  fi
 done
