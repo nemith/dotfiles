ab7b2592d725f12f5d7959ca255fedcda7ec4542
Brnadon Bennett <bennetb@gmail.com>
2016-06-13 14:38:04 -0600

    [powerline] Add python3 support

--- home/.bashrc.d/60powerline.bash
+++ home/.bashrc.d/60powerline.bash
@@ -1,7 +1,10 @@
-powerline_dirs=(
+
+powerline_dirs+=(
+	$(python3 -c "import sys; print(' '.join([p+'/powerline' for p in sys.path[1:]]))")
 	$(python -c "import sys; print ' '.join([p+'/powerline' for p in sys.path[1:]])")
- 	'/usr/share/powerline'
+	'/usr/share/powerline'
 )
+
 for dir in "${powerline_dirs[@]}"; do
 	if [ -d "$dir" ]; then
 		export POWERLINE_DIR=$dir
