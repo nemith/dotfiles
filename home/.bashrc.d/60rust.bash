9fb92ed5567b0895c186e8014aeca60c2242be1a
Brandon Bennett <bennetb@gmail.com>
2016-06-19 21:03:27 -0600

    [rust] Update rust location finder

--- home/.bashrc.d/60rust.bash
+++ home/.bashrc.d/60rust.bash
@@ -1,6 +1,5 @@
 if [ -d /opt/rust ]; then
 	append_path /opt/rust/bin
-	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/rust/lib
 fi
 
-[ ! -x $(whiff rust) ] && return
+[ ! -x $(whiff rustc) ] && return
