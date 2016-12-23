354da2515732962e2bfcc011dd60fd7628786718
Brandon Bennett <bbennett@fb.com>
2016-12-23 14:50:30 -0800

    [dircolors] Move directory

--- home/.bashrc.d/60dircolors.bash
+++ home/.bashrc.d/60dircolors.bash
@@ -1,5 +1,5 @@
 # New dircolors
 
 if [ -x $(whiff dircolors gdircolors) ]; then
-	eval $($(whiff dircolors gdircolors) $HOME/.dircolors/solarized/dircolors.256dark)
+	eval $($(whiff dircolors gdircolors) $HOME/.dircolors.d/solarized/dircolors.256dark)
 fi
