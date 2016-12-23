fab07f190f11136b13401daff6f7364fda082c64
Brandon Bennett <bennetb@gmail.com>
2016-12-23 15:38:42 -0700

    [powerline] Allow powerline rpms to work

--- home/.bashrc.d/60powerline.bash
+++ home/.bashrc.d/60powerline.bash
@@ -1,14 +1,15 @@
 
 powerline_dirs+=(
-	$(python3 -c "import sys; print(' '.join([p+'/powerline' for p in sys.path[1:]]))")
-	$(python -c "import sys; print ' '.join([p+'/powerline' for p in sys.path[1:]])")
 	'/usr/share/powerline'
+	'/usr/share/powerline/bindings'
+	$(python3 -c "import sys; print(' '.join([p+'/powerline/bindings' for p in sys.path[1:]]))")
+	$(python -c "import sys; print ' '.join([p+'/powerline/bindings' for p in sys.path[1:]])")
 )
 
 for dir in "${powerline_dirs[@]}"; do
 	if [ -d "$dir" ]; then
-		export POWERLINE_DIR=$dir
-    source "$POWERLINE_DIR/bindings/bash/powerline.sh"
+	export POWERLINE_DIR=$dir
+    source "$POWERLINE_DIR/bash/powerline.sh"
     break;
   fi
 done
