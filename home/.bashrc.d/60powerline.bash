596388967a476891da978f8eaa56153d7031a986
Brandon Bennett <bbennett@fb.com>
2017-07-05 13:56:02 -0700

    [powerline] only search py3 if it exists

--- home/.bashrc.d/60powerline.bash
+++ home/.bashrc.d/60powerline.bash
@@ -2,9 +2,15 @@
 powerline_dirs+=(
 	'/usr/share/powerline'
 	'/usr/share/powerline/bindings'
-	$(python3 -c "import sys; print(' '.join([p+'/powerline/bindings' for p in sys.path[1:]]))")
 	$(python -c "import sys; print ' '.join([p+'/powerline/bindings' for p in sys.path[1:]])")
 )
+
+if [ -x "$(whiff python3)" ]; then
+  powerline_dirs+=(
+	  $(python3 -c "import sys; print(' '.join([p+'/powerline/bindings' for p in sys.path[1:]]))")
+  )
+fi
+
 
 for dir in "${powerline_dirs[@]}"; do
 	if [ -d "$dir" ]; then
