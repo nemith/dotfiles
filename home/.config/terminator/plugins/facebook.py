3cc142656aa6ea6126127633072ce3728f2e6b96
Brandon Bennett <bennetb@gmail.com>
2018-04-04 10:02:27 -0600

    [terminator] add SEV url handler

--- home/.config/terminator/plugins/facebook.py
+++ home/.config/terminator/plugins/facebook.py
@@ -1,26 +1,44 @@
+import re
 import terminatorlib.plugin as plugin
 
+AVAILABLE = [
+    'FBTasksURLHandler',
+    'FBPhabricatorURLHandler',
+    'FBSEVURLHandler',
+]
 
-AVAILABLE = ['FBTasksURLHandler', 'FBPhabricatorURLHandler']
+INTERN = "https://our.intern.facebook.com/intern/"
+
 
 class FBTasksURLHandler(plugin.URLHandler):
     capabilities = ['url_handler']
     handler_name = 'fb_tasks'
-    match = '\\b([Tt][0-9]+)\\b'
+    match = r'\b([Tt][0-9]+)\b'
     nameopen = "Open Task"
     namecopy = "Copy Task"
 
     def callback(self, url):
         url = url[1:]
-        return('https://our.intern.facebook.com/intern/tasks/?t=%s' % url)
+        return(INTERN + 'tasks/?t=%s' % url)
 
 
 class FBPhabricatorURLHandler(plugin.URLHandler):
     capabilities = ['url_handler']
     handler_name = 'fb_phabricator'
-    match = '\\b([DdPp][0-9]+)\\b'
+    match = r'\b([DdPp][0-9]+)\b'
     nameopen = "Open Phabricator Link"
     namecopy = "Copy Phabricator Link"
 
     def callback(self, url):
         return('https://phabricator.fb.com/%s' % url.upper())
+
+class FBSEVURLHandler(plugin.URLHandler):
+    capabilities = ['url_handler']
+    handler_name = 'fb_sev'
+    match = r'\b[sS]([0-9]+)\b'
+    nameopen = "Open SEV"
+    namecopy = "Copy SEV Link"
+
+    def callback(self, url):
+        m = re.match(r'[sS]([0-9]+)', url)
+        return INTERN + 'sevmanager/view/s/%s' % m.group(1)
